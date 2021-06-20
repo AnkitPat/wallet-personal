c6ab512b2bb70d160ae252f99351e181fa0175c1
Mayur <mayur.gaud@easternenterprise.com>
2021-06-20 17:36:34 +0530

    Fixing subscription

--- src/views/membership/index.js
+++ src/views/membership/index.js
@@ -1,35 +1,33 @@
-import { useState, useEffect, Fragment } from 'react'
-import axios from 'axios'
+import {useState, useEffect, Fragment} from 'react'
 import PricingFaqs from './PricingFaqs'
 import PricingCards from './PricingCards'
-import PricingTrial from './PricingTrial'
 import PricingHeader from './PricingHeader'
 
 import '@styles/base/pages/page-pricing.scss'
-import { useDispatch } from 'react-redux'
-import { fetchPlans } from '../../redux/actions/membership'
+import {useDispatch, useSelector} from 'react-redux'
+import {fetchPlans} from '../../redux/actions/membership'
 
 const Pricing = () => {
-
-
-  const [data, setData] = useState(null),
-    [faq, setFaq] = useState(null),
-    [duration, setDuration] = useState('monthly')
+    const [data, setData] = useState(null),
+        [faq, setFaq] = useState(null),
+        [duration, setDuration] = useState('monthly')
 
     const dispatch = useDispatch()
-  useEffect(() => {
-      dispatch(fetchPlans())
-  }, [])
+    useEffect(() => {
+        dispatch(fetchPlans())
+    }, [])
 
-  return (
-    <div id='pricing-table'>
-      <PricingHeader duration={duration} setDuration={setDuration} />
-        <Fragment>
-          <PricingCards duration={duration} />
-          <PricingFaqs data={faq} />
-        </Fragment>
-    </div>
-  )
+    const plans = useSelector(state => state.membership.plans)
+
+    return (
+        <div id='pricing-table'>
+            <PricingHeader duration={duration} setDuration={setDuration}/>
+            <Fragment>
+                <PricingCards plans={plans} duration={duration}/>
+                <PricingFaqs data={faq}/>
+            </Fragment>
+        </div>
+    )
 }
 
 export default Pricing
