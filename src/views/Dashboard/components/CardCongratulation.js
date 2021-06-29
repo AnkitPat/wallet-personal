9e9fc86bbff2ef905402dad9ad543bc082677e1f
Ankit Patidar <a.patidar@easternenterprise.com>
2021-06-29 22:02:22 +0530

    Commit after changes for subscription UI

--- src/views/Dashboard/components/CardCongratulation.js
+++ src/views/Dashboard/components/CardCongratulation.js
@@ -12,9 +12,9 @@ const CardCongratulations = () => {
       <CardBody className='text-center'>
         <img className='congratulations-img-left' src={decorationLeft} alt='decor-left' />
         <img className='congratulations-img-right' src={decorationRight} alt='decor-right' />
-        <Avatar icon={<Award size={28} />} className='shadow' color='primary' size='xl' />
+        <div className="h2">🎉</div>
         <div className='text-center'>
-          <h1 className='mb-1 text-white'>Congratulations {userDetails ? userDetails.name : ''},</h1>
+          <h1 className='mb-1 text-white'>Welcome To Potentiam {userDetails ? userDetails.name : ''},</h1>
           <CardText className='m-auto w-75'>
           Get Ready For The Upcoming Featured Projects On Potentiam
           </CardText>
