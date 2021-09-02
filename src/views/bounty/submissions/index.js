502275b1f244657e536027f0e3d516084e4ad4c5
Ankit Patidar <a.patidar@easternenterprise.com>
2021-09-02 21:07:45 +0530

    Submision file revert to development

--- src/views/bounty/submissions/index.js
+++ src/views/bounty/submissions/index.js
@@ -1,13 +1,13 @@
 import moment from 'moment'
-import React, { useEffect } from 'react'
+import React, {useEffect} from 'react'
 import DataTable from 'react-data-table-component'
-import { useDispatch, useSelector } from 'react-redux'
-import { UncontrolledTooltip } from 'reactstrap'
-import { ProgressLoader } from '../../../layouts/ProgressLoader'
-import { fetchSubmission, verifyBounty } from '../../../redux/actions/bounty'
+import {useDispatch, useSelector} from 'react-redux'
+import {UncontrolledTooltip} from 'reactstrap'
+import {ProgressLoader} from '../../../layouts/ProgressLoader'
+import {fetchSubmission, verifyBounty} from '../../../redux/actions/bounty'
 import '@styles/react/libs/tables/react-dataTable-component.scss'
-import { Text } from 'recharts'
-import { Check, X } from "react-feather"
+import {Text} from 'recharts'
+import {Check, X} from "react-feather"
 
 const Submissions = () => {
 
@@ -24,31 +24,43 @@ const Submissions = () => {
         {
             selector: 'bountyTask_title',
             name: 'Title',
-            width: '20%',
+            style: {
+                width: '20%'
+            },
             headerStyle: {
+                width: '20%'
             }
         },
         {
             selector: 'user_bounties_result',
             name: 'Submitted Link',
-            cell: row => <a style={{ textOverflow: 'ellipsis', overflow: 'hidden', whiteSpace: 'nowrap' }} href={row.user_bounties_result} target="_blank">{row.user_bounties_result}</a>,
-            width: '20%',
+            cell: row => <a style={{textOverflow: 'ellipsis', overflow:'hidden', whiteSpace: 'nowrap'}} href={row.user_bounties_result}  target="_blank">{row.user_bounties_result}</a>,
+            style: {
+                width: '20%'
+            },
             headerStyle: {
+                width: '20%'
             }
         },
         {
             selector: 'bountyTiers_reward',
             name: 'Reward',
-            width: '10%',
+            style: {
+                width: '20%'
+            },
             headerStyle: {
+                width: '20%'
             }
         },
         {
             selector: 'bountyTask_updatedAt',
             name: 'Last updated',
             format: row => moment(row.bountyTask_updatedAt).format('DD/MM/YYYY'),
-            width: '15%',
+            style: {
+                width: '20%'
+            },
             headerStyle: {
+                width: '20%'
             }
         },
         {
@@ -57,8 +69,11 @@ const Submissions = () => {
             cell: row => <div>
                 {row.user_name}
             </div>,
-            width: '20%',
+            style: {
+                width: '20%'
+            },
             headerStyle: {
+                width: '20%'
             }
         },
         {
@@ -67,28 +82,31 @@ const Submissions = () => {
             cell: row => (
                 <div className='column-action d-flex align-items-center'>
                     <span className="cursor-pointer" onClick={() => dispatch(verifyBounty(row.user_bounties_id, true))}>
-                        <Check size={17} id={`send-tooltip-${row.user_bounties_id}`} />
+                        <Check size={17} id={`send-tooltip-${row.user_bounties_id}`}/>
                         <UncontrolledTooltip placement='top' target={`send-tooltip-${row.user_bounties_id}`}>
                             Accept
                         </UncontrolledTooltip>
                     </span>
                     <span className="cursor-pointer" onClick={() => dispatch(verifyBounty(row.user_bounties_id, false))}>
-                        <X size={17} className='mx-1' id={`pw-tooltip-${row.user_bounties_id}`} />
+                        <X size={17} className='mx-1' id={`pw-tooltip-${row.user_bounties_id}`}/>
                         <UncontrolledTooltip placement='top' target={`pw-tooltip-${row.user_bounties_id}`}>
                             Reject
                         </UncontrolledTooltip>
                     </span>
                 </div>
             ),
-            width: '10%',
+            style: {
+                width: '20%'
+            },
             headerStyle: {
+                width: '20%'
             }
         }
     ]
 
     return (
         <div>
-            {loading ? <ProgressLoader size='lg' /> : <div className="mt-4">
+            {loading ? <ProgressLoader size='lg'/> : <div className="mt-4">
                 <div className="mb-2"><Text className="h1 text-primary">Submissions</Text></div>
                 <DataTable
                     noHeader
